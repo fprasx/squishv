51a8444d438165be47c4c17c6a5c381bb0a5c1ad
Felix Prasanna <fpx@mit.edu>
2023-04-01 16:28:24 -0400

    add lexer apis, begin parsing

--- riscv/src/main.rs
+++ riscv/src/main.rs
@@ -1,7 +1,10 @@
-use riscv::parse_v2::Lexer;
+use riscv::parse_v2::{assembly, Lexer};
+
 fn main() {
     let lexer = Lexer::new("addi x0, sp, 10 beqz");
     for token in lexer {
         println!("{token:?}")
     }
+    let addi = assembly("addi s0, t1, -0x123");
+    println!("{addi:?}")
 }
--- riscv/src/parse_v2.rs
+++ riscv/src/parse_v2.rs
@@ -1,6 +1,10 @@
-use std::str::FromStr;
+use std::{fmt::Display, str::FromStr};
 
 use serde::{Deserialize, Serialize};
+
+const REG_IMM: [&str; 9] = [
+    "addi", "slti", "sltiu", "xori", "ori", "andi", "slli", "srli", "srai",
+];
 
 #[allow(non_camel_case_types)]
 #[rustfmt::skip]
@@ -12,18 +16,20 @@ pub enum Register {
     s0, s1, s2, s3, s4, s5, s6, s7, s8, s9, s10, s11,
 }
 
+#[allow(non_camel_case_types)]
+
 macro_rules! register_parse_impl {
     ($( ($reg:ident = $xreg:ident) )*) => {
         impl FromStr for Register {
-            type Err = &'static str;
-            fn from_str(s: &str) -> Result<Register, &'static str> {
+            type Err = String;
+            fn from_str(s: &str) -> Result<Register, Self::Err> {
                 match s.trim() {
                     $(
                         stringify!($reg) | stringify!($xreg)
                             => Ok(Register::$reg),
                     )*
                     "zero" => Ok(Register::x0),
-                    _ => Err("unrecognized register")
+                    unknown => Err(format!("unrecognized register {unknown}"))
                 }
             }
         }
@@ -42,73 +48,144 @@ register_parse_impl! {
    (s7 = x23) (s8 = x24) (s9 = x25) (s10 = x26) (s11 = x27)
 }
 
+// All the empty parenthesese might look a little funky but they are there to
+// make macro implementations nicer.
 #[derive(Debug, Clone, Copy)]
 pub enum Token<'a> {
-    RightParen,
-    LeftParen,
-    Comma,
-    Colon,
+    RightParen(),
+    LeftParen(),
+    Comma(),
+    Colon(),
+    Minus(),
     Constant(i32),
     Ident(&'a str),
 }
 
-pub struct Lexer<'a> {
-    buf: &'a str,
+// Functions for attempting to get a certain token from the lexer. If the next
+// token is the desired token, the stream will be advanced and the token will be
+// returned. Otherwise the stream is not advanced.
+macro_rules! parse_token {
+    ($($tokentype:ident => $tokenfn:ident)+) => {
+        $(
+            impl<'a> Lexer<'a> {
+                pub fn $tokenfn(&mut self) -> Result<Token<'a>, String> {
+                    match self.peek() {
+                        // Unwrap is safe as we already peeked, we just use next
+                        // to advance the stream
+                        Some(Token::$tokentype(..)) => Ok(self.next().unwrap()),
+                        Some(other) => Err(format!("Expected {}, found {other}", stringify!($tokentype))),
+                        None => Err(format!("Expected {}, but ran out of input", stringify!($tokentype))),
+                    }
+                }
+            }
+        )*
+    };
 }
 
-impl<'a> Lexer<'a> {
-    pub fn new(buf: &'a str) -> Self {
-        Lexer {
-            buf,
+// Keep this in sync with the definition of Token
+parse_token! {
+    Comma => comma
+    Colon => colon
+    RightParen => right_paren
+    LeftParen  => left_paren
+    Ident => ident
+    Constant => constant
+    Minus => minus
+}
+
+impl<'a> Token<'a> {
+    pub fn unwrap_ident(self) -> &'a str {
+        match self {
+            Token::Ident(inner) => inner,
+            other => panic!("called unwrap ident on a {other}"),
+        }
+    }
+
+    pub fn unwrap_constant(self) -> i32 {
+        match self {
+            Token::Constant(inner) => inner,
+            other => panic!("called unwrap ident on a {other}"),
         }
     }
 }
 
-impl<'a> Iterator for Lexer<'a> {
-    type Item = Token<'a>;
+/// Peekable lexer that yields tokens one at a time.
+pub struct Lexer<'a> {
+    buf: &'a str,
+    peek: Option<Token<'a>>,
+}
 
-    fn next(&mut self) -> Option<Self::Item> {
+impl<'a> Lexer<'a> {
+    pub fn new(buf: &'a str) -> Self {
+        let mut lexer = Lexer { buf, peek: None };
+        // Initalize the peek with the first token from the stream
+        lexer.next();
+        lexer
+    }
+
+    // Get the next token from the internal buffer.
+    fn next_from_buf(&mut self) -> Option<Token<'a>> {
         self.buf = self.buf.trim();
         if self.buf.is_empty() {
             return None;
         }
         if let Some(rest) = self.buf.strip_prefix('(') {
             self.buf = rest;
-            return Some(Token::LeftParen);
+            return Some(Token::LeftParen());
         } else if let Some(rest) = self.buf.strip_prefix(')') {
             self.buf = rest;
-            return Some(Token::RightParen);
+            return Some(Token::RightParen());
         } else if let Some(rest) = self.buf.strip_prefix(',') {
             self.buf = rest;
-            return Some(Token::Comma);
+            return Some(Token::Comma());
         } else if let Some(rest) = self.buf.strip_prefix(':') {
             self.buf = rest;
-            return Some(Token::Colon);
+            return Some(Token::Colon());
+        } else if let Some(rest) = self.buf.strip_prefix('-') {
+            self.buf = rest;
+            return Some(Token::Minus());
         } else if let Some(rest) = self.buf.strip_prefix("0x") {
             if let Some((digits, rem)) = rest.consume(|c| c.is_ascii_hexdigit()) {
                 return match parse_int::parse::<i32>(&format!("0x{}", digits)) {
                     Ok(number) => {
                         self.buf = rem;
-                        return Some(Token::Constant(number));
+                        Some(Token::Constant(number))
                     }
                     Err(_) => None,
                 };
+            } else {
+                None
             }
-        }
-        if let Some((digits, rest)) = self.buf.consume(|c| c.is_ascii_digit()) {
-            match parse_int::parse::<i32>(digits) {
+        } else if let Some((digits, rest)) = self.buf.consume(|c| c.is_ascii_digit()) {
+            return match parse_int::parse::<i32>(digits) {
                 Ok(number) => {
                     self.buf = rest;
                     Some(Token::Constant(number))
                 }
                 Err(_) => None,
-            }
+            };
         } else if let Some((label, rest)) = self.buf.consume(char::is_alphanumeric) {
             self.buf = rest;
             Some(Token::Ident(label))
         } else {
             None
         }
+    }
+
+    /// Return the next token, but don't advance the lexer.
+    pub fn peek(&mut self) -> Option<Token<'a>> {
+        self.peek
+    }
+}
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = Token<'a>;
+
+    /// Return the next token and advance the lexer.
+    fn next(&mut self) -> Option<Self::Item> {
+        let old = self.peek;
+        self.peek = self.next_from_buf();
+        old
     }
 }
 
@@ -128,5 +205,136 @@ impl<'a> Consume<'a> for &'a str {
             .take_while(|(_, c)| predicate(*c))
             .last()
             .map(|(index, _)| (&self[..index + 1], &self[index + 1..]))
+    }
+}
+
+#[rustfmt::skip]
+#[allow(non_camel_case_types)]
+#[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone)]
+pub enum Instruction<'a> {
+    // Register immediate
+    addi  { rd: Register, r1: Register, imm: i32, },
+    slti  { rd: Register, r1: Register, imm: i32, },
+    sltiu { rd: Register, r1: Register, imm: i32, },
+    xori  { rd: Register, r1: Register, imm: i32, },
+    ori   { rd: Register, r1: Register, imm: i32, },
+    andi  { rd: Register, r1: Register, imm: i32, },
+    slli  { rd: Register, r1: Register, imm: i32, },
+    srli  { rd: Register, r1: Register, imm: i32, },
+    srai  { rd: Register, r1: Register, imm: i32, },
+
+    // Loady bois
+    lui { rd: Register, imm: i32, },
+    li  { rd: Register, imm: i32, },
+
+    // Register register
+    add  { rd: Register, r1: Register, r2: Register, },
+    sub  { rd: Register, r1: Register, r2: Register, },
+    sll  { rd: Register, r1: Register, r2: Register, },
+    slt  { rd: Register, r1: Register, r2: Register, },
+    sltu { rd: Register, r1: Register, r2: Register, },
+    xor  { rd: Register, r1: Register, r2: Register, },
+    srl  { rd: Register, r1: Register, r2: Register, },
+    sra  { rd: Register, r1: Register, r2: Register, },
+    or   { rd: Register, r1: Register, r2: Register, },
+    and  { rd: Register, r1: Register, r2: Register, },
+
+    // Memory
+    lw { rd: Register, offset: i32, r1: Register  },
+    sw { r2: Register, offset: i32, r1: Register },
+
+    // Branches + some fake branches
+    beq  { r1: Register, r2: Register, label: &'a str },
+    bne  { r1: Register, r2: Register, label: &'a str },
+    blt  { r1: Register, r2: Register, label: &'a str },
+    bge  { r1: Register, r2: Register, label: &'a str },
+    bltu { r1: Register, r2: Register, label: &'a str },
+    bgeu { r1: Register, r2: Register, label: &'a str },
+    bgt  { r1: Register, r2: Register, label: &'a str },
+    ble  { r1: Register, r2: Register, label: &'a str },
+    bgtu { r1: Register, r2: Register, label: &'a str },
+    bleu { r1: Register, r2: Register, label: &'a str },
+
+    // 0-branches
+    beqz { r1: Register, label: &'a str },
+    bnez { r1: Register, label: &'a str },
+    bltz { r1: Register, label: &'a str },
+    bgez { r1: Register, label: &'a str },
+    bgtz { r1: Register, label: &'a str },
+    blez { r1: Register, label: &'a str },
+
+    // Unaries
+    mv  { rd: Register, r1: Register },
+    not { rd: Register, r1: Register },
+    neg { rd: Register, r1: Register },
+
+    // Calling and jumping
+    jal         { rd: Register, label: &'a str },
+    jalr        { rd: Register, offset: i32, r1: Register },
+    call        { label: &'a str },
+    pseudo_jal  { label: &'a str },
+    j           { label: &'a str },
+    jr          { rs: Register },
+    pseudo_jalr { rs: Register },
+    ret         {},
+}
+
+/// An item of RISC-V assembly, either an instruction or label (for now)
+#[derive(Serialize, Deserialize, Eq, PartialEq, Debug, Clone)]
+pub enum Item<'a> {
+    #[serde(borrow)]
+    Instruction(Instruction<'a>),
+    Label(&'a str),
+}
+
+pub fn assembly(source: &str) -> Result<Item, String> {
+    let mut tokens = Lexer::new(source);
+    let ident = tokens.ident()?.unwrap_ident();
+    if let Ok(Token::Colon()) = tokens.colon() {
+        return Ok(Item::Label(ident));
+    }
+    if REG_IMM.contains(&ident) {
+        reg_imm_args(&mut tokens).map(Item::Instruction)
+    } else {
+        todo!()
+    }
+}
+
+fn reg_imm_args<'a>(tokens: &mut Lexer<'a>) -> Result<Instruction<'a>, String> {
+    let rd: Register = tokens.ident()?.try_into()?;
+    let _ = tokens.comma()?;
+    let r1: Register = tokens.ident()?.try_into()?;
+    let _ = tokens.comma()?;
+    let negative = tokens.minus().is_ok();
+    let imm: i32 = tokens.constant()?.unwrap_constant();
+    Ok(Instruction::addi {
+        rd,
+        r1,
+        imm: if negative { -imm } else { imm },
+    })
+}
+
+impl Display for Token<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Token::RightParen() => write!(f, "("),
+            Token::LeftParen() => write!(f, ")"),
+            Token::Comma() => write!(f, ","),
+            Token::Colon() => write!(f, ":"),
+            Token::Minus() => write!(f, "-"),
+            Token::Constant(num) => write!(f, "{}", num),
+            Token::Ident(ident) => write!(f, "{}", ident),
+        }
+    }
+}
+
+impl TryFrom<Token<'_>> for Register {
+    type Error = String;
+
+    fn try_from(value: Token) -> Result<Self, Self::Error> {
+        match value {
+            Token::Ident(ident) => ident.parse(),
+            other => Err(format!("Expected register, found {other}")),
+        }
     }
 }
