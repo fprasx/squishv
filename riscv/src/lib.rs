9df7fb857ab0d611d080cafbbd5b292994dc624f
Felix Prasanna <fpx@mit.edu>
2023-08-10 17:16:49 -0400

    remove unnecessary wasm deps

--- riscv/src/lib.rs
+++ riscv/src/lib.rs
@@ -1,19 +1,6 @@
-use wasm_bindgen::prelude::*;
-
 pub mod executor;
 pub mod lex;
 pub mod parse;
-
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
-#[wasm_bindgen]
-pub fn helloworld() -> JsValue {
-    serde_wasm_bindgen::to_value(&"Hello from Rust!").unwrap()
-}
 
 // vec! like syntax for a hashmap
 #[macro_export]
