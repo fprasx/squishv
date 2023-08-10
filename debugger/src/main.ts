d481c3decc4ae950f2c6634fb4d33e0e56b5dfe6
Felix Prasanna <fpx@mit.edu>
2023-08-10 17:10:32 -0400

    remove svelte

--- debugger/src/main.ts
+++
@@ -1,8 +0,0 @@
-import App from './App.svelte';
-
-const app = new App({
-    // Safe since we know it exists
-    target: document.getElementById('app') as Element,
-});
-
-export default app;
--- debugger/src/stores/source.ts
+++
@@ -1,3 +0,0 @@
-import { writable } from 'svelte/store';
-
-export const source = writable('addi a0, x0, 42');
