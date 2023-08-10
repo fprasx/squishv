07ca8bf4a1a5ba47348c403e5d44d86efe48904e
Felix Prasanna <fpx@mit.edu>
2023-08-09 21:12:26 -0400

    add executor module

---
+++ riscv/src/executor/memory.rs
@@ -0,0 +1,276 @@
+use std::collections::HashMap;
+
+use anyhow::{bail, ensure, Context};
+
+use crate::parse::{LoadOp, StoreOp};
+
+#[derive(Debug, PartialEq, Eq, Default)]
+pub struct Config {
+    /// The default value for unitialized memory locations. `None` means there is
+    /// not default value and it is an error to access an unitialized location.
+    /// Defaults to `None`.
+    default_value: Option<u8>,
+
+    /// Whether or not to allow unaligned accesses. Defaults to `false`.
+    allow_unaligned: bool,
+}
+
+impl Config {
+    pub fn new(default_value: Option<u8>, allow_unaligned: bool) -> Self {
+        Self {
+            default_value,
+            allow_unaligned,
+        }
+    }
+}
+
+#[derive(Debug, Default, PartialEq, Eq)]
+/// Byte addressible memory that handles unitialized values and unaligned access.
+///
+/// See [`Config`] for more details.
+pub struct Memory {
+    config: Config,
+
+    // Bytes are stored in little-endian order
+    mem: HashMap<i32, u8>,
+}
+
+impl Memory {
+    pub fn load(&self, addr: i32, op: LoadOp) -> anyhow::Result<i32> {
+        match op {
+            LoadOp::Lw => {
+                if !self.config.allow_unaligned {
+                    ensure!(addr & 0b11 == 0, "uanligned access: address = {addr:#010x}");
+                }
+                let data = self
+                    .load_bytes::<4>(addr)
+                    .with_context(|| format!("failed to load word at {addr:}"))?;
+                Ok(i32::from_le_bytes(data))
+            }
+            LoadOp::Lh => {
+                if !self.config.allow_unaligned {
+                    ensure!(addr & 0b1 == 0, "unaligned access: address = {addr:#010x}");
+                }
+                let data = self
+                    .load_bytes::<2>(addr)
+                    .with_context(|| format!("failed to load half word at {addr:}"))?;
+                // Sign extends
+                Ok(i16::from_le_bytes(data) as i32)
+            }
+            LoadOp::Lhu => {
+                if !self.config.allow_unaligned {
+                    ensure!(addr & 0b1 == 0, "uanligned access: address = {addr:#010x}");
+                }
+                let data = self
+                    .load_bytes::<2>(addr)
+                    .with_context(|| format!("failed to load half word at {addr:}"))?;
+                // First cast to u32 to zero extend, then cast to i32
+                Ok((u16::from_le_bytes(data) as u32) as i32)
+            }
+            LoadOp::Lb => {
+                let data = self
+                    .load_bytes::<1>(addr)
+                    .with_context(|| format!("failed to load byte at {addr:}"))?;
+                // Sign extends
+                Ok(i8::from_le_bytes(data) as i32)
+            }
+            LoadOp::Lbu => {
+                let data = self
+                    .load_bytes::<1>(addr)
+                    .with_context(|| format!("failed to load byte at {addr:}"))?;
+                // First cast to u32 to zero extend, then cast to i32
+                Ok((u8::from_le_bytes(data) as u32) as i32)
+            }
+        }
+    }
+
+    /// Load `N` bytes, starting at the base address. Returns an error if any of
+    /// then is unitialized.
+    fn load_bytes<const N: usize>(&self, base_addr: i32) -> anyhow::Result<[u8; N]> {
+        let mut data = [0u8; N];
+        for (offset, spot) in data.iter_mut().enumerate() {
+            let addr = base_addr + (offset as i32);
+            let Some(byte) = self.mem.get(&addr).copied().or(self.config.default_value) else {
+                bail!("access to unitialized address: {addr:#010x}")
+            };
+            *spot = byte;
+        }
+        Ok(data)
+    }
+
+    /// Store `N` bytes, starting at the base address
+    fn store_bytes<const N: usize>(&mut self, base_addr: i32, bytes: [u8; N]) {
+        for (offset, byte) in bytes.iter().enumerate() {
+            let addr = base_addr + (offset as i32);
+            self.mem.insert(addr, *byte);
+        }
+    }
+
+    /// Store a value at a certain address, returning the value that was previously
+    /// there, if any.
+    pub fn store(&mut self, addr: i32, val: i32, op: StoreOp) -> anyhow::Result<()> {
+        // Note: casting to a smaller integer type truncates, which is what we want
+        match op {
+            StoreOp::Sw => {
+                if !self.config.allow_unaligned {
+                    ensure!(addr & 0b11 == 0, "uanligned access: address = {addr:#010x}");
+                }
+                self.store_bytes(addr, val.to_le_bytes());
+            }
+            StoreOp::Sh => {
+                if !self.config.allow_unaligned {
+                    ensure!(addr & 0b1 == 0, "uanligned access: address = {addr:#010x}");
+                }
+                self.store_bytes(addr, (val as i16).to_le_bytes());
+            }
+            StoreOp::Sb => {
+                self.store_bytes(addr, (val as i8).to_le_bytes());
+            }
+        }
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        map,
+        parse::{LoadOp, StoreOp},
+    };
+
+    use super::*;
+
+    #[test]
+    fn loads() {
+        let mut mem: Memory = Default::default();
+        assert_eq!(
+            Memory {
+                config: Config {
+                    default_value: None,
+                    allow_unaligned: false
+                },
+                mem: map![]
+            },
+            mem
+        );
+        assert!(mem.load(0x0, LoadOp::Lw).is_err());
+        assert!(mem.load(0x0, LoadOp::Lh).is_err());
+        assert!(mem.load(0x0, LoadOp::Lb).is_err());
+        assert!(mem.load(0x0, LoadOp::Lhu).is_err());
+        assert!(mem.load(0x0, LoadOp::Lbu).is_err());
+
+        mem.store(0x40, 0x1234abcd, StoreOp::Sw).unwrap();
+
+        assert_eq!(mem.load(0x40, LoadOp::Lw).unwrap(), 0x1234abcd);
+        // out of bounds
+        assert!(mem.load(0x41, LoadOp::Lw).is_err());
+
+        assert_eq!(mem.load(0x40, LoadOp::Lh).unwrap(), 0xffffabcd_u32 as i32);
+        assert!(mem.load(0x41, LoadOp::Lh).is_err());
+        assert_eq!(mem.load(0x42, LoadOp::Lh).unwrap(), 0x00001234_u32 as i32);
+        // out of bounds
+        assert!(mem.load(0x43, LoadOp::Lh).is_err());
+
+        assert_eq!(mem.load(0x40, LoadOp::Lb).unwrap(), 0xffffffcd_u32 as i32);
+        assert_eq!(mem.load(0x41, LoadOp::Lb).unwrap(), 0xffffffab_u32 as i32);
+        assert_eq!(mem.load(0x42, LoadOp::Lb).unwrap(), 0x00000034_u32 as i32);
+        assert_eq!(mem.load(0x43, LoadOp::Lb).unwrap(), 0x00000012_u32 as i32);
+        // out of bounds
+        assert!(mem.load(0x44, LoadOp::Lb).is_err());
+
+        assert_eq!(mem.load(0x40, LoadOp::Lhu).unwrap(), 0x0000abcd);
+        assert!(mem.load(0x41, LoadOp::Lhu).is_err());
+        assert_eq!(mem.load(0x42, LoadOp::Lhu).unwrap(), 0x00001234);
+        // out of bounds
+        assert!(mem.load(0x43, LoadOp::Lhu).is_err());
+
+        assert_eq!(mem.load(0x40, LoadOp::Lbu).unwrap(), 0x000000cd);
+        assert_eq!(mem.load(0x41, LoadOp::Lbu).unwrap(), 0x000000ab);
+        assert_eq!(mem.load(0x42, LoadOp::Lbu).unwrap(), 0x00000034);
+        assert_eq!(mem.load(0x43, LoadOp::Lbu).unwrap(), 0x00000012);
+        // out of bounds
+        assert!(mem.load(0x44, LoadOp::Lbu).is_err());
+
+        // Unaligned access
+        mem.config.allow_unaligned = true;
+        mem.store(0x44, 0x1234abcd, StoreOp::Sw).unwrap();
+        assert_eq!(mem.load(0x41, LoadOp::Lw).unwrap(), 0xcd1234ab_u32 as i32);
+        assert_eq!(mem.load(0x41, LoadOp::Lh).unwrap(), 0x000034ab_u32 as i32);
+        assert_eq!(mem.load(0x41, LoadOp::Lhu).unwrap(), 0x000034ab);
+
+        // Default values
+        mem.config.default_value = Some(0xaa);
+        assert_eq!(mem.load(0x48, LoadOp::Lw).unwrap(), 0xaaaaaaaa_u32 as i32);
+        assert_eq!(mem.load(0x48, LoadOp::Lh).unwrap(), 0xffffaaaa_u32 as i32);
+        assert_eq!(mem.load(0x48, LoadOp::Lhu).unwrap(), 0xaaaa);
+        assert_eq!(mem.load(0x48, LoadOp::Lb).unwrap(), 0xffffffaa_u32 as i32);
+        assert_eq!(mem.load(0x48, LoadOp::Lbu).unwrap(), 0xaa);
+    }
+
+    #[test]
+    fn stores() {
+        let mut mem: Memory = Default::default();
+
+        assert_eq!(mem.mem, map![]);
+
+        mem.store(0x20, 0x1234abcd, StoreOp::Sw).unwrap();
+        assert_eq!(
+            mem.mem,
+            map![
+                0x20 => 0xcd,
+                0x21 => 0xab,
+                0x22 => 0x34,
+                0x23 => 0x12,
+            ]
+        );
+        // Truncates
+        mem.store(0x22, 0x1234abcd, StoreOp::Sh).unwrap();
+        assert_eq!(
+            mem.mem,
+            map![
+                0x20 => 0xcd,
+                0x21 => 0xab,
+                0x22 => 0xcd,
+                0x23 => 0xab,
+            ]
+        );
+        mem.store(0x23, 0x1234abcd, StoreOp::Sb).unwrap();
+        assert_eq!(
+            mem.mem,
+            map![
+                0x20 => 0xcd,
+                0x21 => 0xab,
+                0x22 => 0xcd,
+                0x23 => 0xcd,
+            ]
+        );
+
+        // Unaligned
+        assert!(mem.store(0x21, 0x1234abcd, StoreOp::Sw).is_err());
+        assert!(mem.store(0x21, 0x1234abcd, StoreOp::Sh).is_err());
+        mem.config.allow_unaligned = true;
+        mem.store(0x21, 0x1234abcd, StoreOp::Sw).unwrap();
+        assert_eq!(
+            mem.mem,
+            map![
+                0x20 => 0xcd,
+                0x21 => 0xcd,
+                0x22 => 0xab,
+                0x23 => 0x34,
+                0x24 => 0x12,
+
+            ]
+        );
+        mem.store(0x23, 0x1234abcd, StoreOp::Sh).unwrap();
+        assert_eq!(
+            mem.mem,
+            map![
+                0x20 => 0xcd,
+                0x21 => 0xcd,
+                0x22 => 0xab,
+                0x23 => 0xcd,
+                0x24 => 0xab,
+            ]
+        );
+    }
+}
---
+++ riscv/src/executor/mod.rs
@@ -0,0 +1,452 @@
+pub mod memory;
+
+// TODO: change all printing to hex
+use std::{
+    collections::HashMap,
+    ops::{Index, IndexMut},
+};
+
+use anyhow::{anyhow, Context, Ok};
+
+use crate::parse::{
+    BranchOp, BranchZeroOp, Instruction, LoadImmOp, Program, RegImmOp, RegRegOp, Register, StoreOp,
+    UnaryOp,
+};
+
+/// Take a snapshot of the registers every `SNAPSHOT_INTERVAL` instructions.
+pub const SNAPSHOT_INTERVAL: usize = 1000;
+
+/// A snapshot of the registers at one point in time.
+#[rustfmt::skip]
+#[derive(Default, Clone, PartialEq, Eq, Debug)]
+struct RegisterSnapshot {
+    pc: i32,
+    
+    x0: i32, ra: i32, sp: i32, gp: i32, tp: i32,
+    
+    t0: i32, t1: i32, t2: i32, t3: i32,
+    t4: i32, t5: i32, t6: i32,
+    
+    a0: i32, a1: i32, a2: i32, a3: i32,
+    a4: i32, a5: i32, a6: i32, a7: i32,
+    
+    s0: i32, s1: i32, s2: i32, s3: i32, s4: i32,
+    s5: i32, s6: i32, s7: i32, s8: i32, s9: i32,
+    s10: i32, s11: i32,
+}
+
+impl RegisterSnapshot {
+    pub fn pc(&self) -> i32 {
+        self.pc
+    }
+}
+
+/// Implements `Index{Mut}<Register>` for RegisterFile and RegisterSnapshot
+macro_rules! register_index_impl {
+    ($( $reg:ident )*) => {
+        impl Index<Register> for RegisterSnapshot {
+            type Output = i32;
+            fn index(&self, index: Register) -> &Self::Output {
+                match index {
+                    $(
+                        Register::$reg
+                            => &self.$reg,
+                    )*
+                }
+            }
+        }
+        impl IndexMut<Register> for RegisterSnapshot {
+            fn index_mut(&mut self, index: Register) -> &mut Self::Output {
+                match index {
+                    $(
+                        Register::$reg
+                            => &mut self.$reg,
+                    )*
+                }
+            }
+        }
+        impl Index<&Register> for RegisterSnapshot {
+            type Output = i32;
+            fn index(&self, index: &Register) -> &Self::Output {
+                match index {
+                    $(
+                        Register::$reg
+                            => &self.$reg,
+                    )*
+                }
+            }
+        }
+        impl IndexMut<&Register> for RegisterSnapshot {
+            fn index_mut(&mut self, index: &Register) -> &mut Self::Output {
+                match index {
+                    $(
+                        Register::$reg
+                            => &mut self.$reg,
+                    )*
+                }
+            }
+        }
+    }
+}
+
+register_index_impl! {
+    x0 ra sp gp tp
+    t0 t1 t2 t3 t4 t5 t6
+    a0 a1 a2 a3 a4 a5 a6 a7
+    s0 s1 s2 s3 s4 s5 s6 s7 s8 s9 s10 s11
+}
+
+#[derive(Debug)]
+pub enum OverflowBehaviour {
+    Wrap,
+    Saturate,
+    // TODO: might not be the correct word
+    Trap,
+}
+
+#[derive(Debug)]
+struct Config {
+    /// What to do when overflow happens.
+    overflow_mode: OverflowBehaviour,
+}
+
+#[derive(Debug)]
+pub struct Executor<'a> {
+    config: Config,
+
+    /// The PC of the next instruction to execute
+    pc: i32,
+
+    /// The number of instructions executed - used to uniquely identify points
+    /// in program execution.
+    executed: usize,
+    program: Program<'a>,
+    regfile: RegisterSnapshot,
+
+    /// Used to store snapshots of the programs state at a certain point in time
+    /// for ttd (time-travel debugging)
+    snapshots: HashMap<usize, RegisterSnapshot>,
+    memory: memory::Memory,
+}
+
+/// An update that should be applied to the Executor after executing an instruction
+#[derive(Debug)]
+pub struct Update {
+    nextpc: i32,
+    diff: Option<Diff>,
+}
+
+impl Update {
+    /// Don't change a register, just jump to the given `pc`
+    fn jump(nextpc: i32) -> Self {
+        Update { nextpc, diff: None }
+    }
+}
+
+/// A diff to apply to the registers or pc
+#[derive(Debug)]
+enum Diff {
+    Memory { addr: i32, val: i32, op: StoreOp },
+    Register { reg: Register, val: i32 },
+}
+
+impl<'a> Executor<'a> {
+    pub fn new(program: Program<'a>) -> Self {
+        Self {
+            config: Config {
+                overflow_mode: OverflowBehaviour::Trap,
+            },
+            pc: 0,
+            executed: 0,
+            program,
+            regfile: Default::default(),
+            snapshots: Default::default(),
+            memory: Default::default(),
+        }
+    }
+
+    /// Adds two numbers while respecting the configuration for overflow behaviour.
+    fn add(&self, fst: i32, snd: i32) -> anyhow::Result<i32> {
+        match self.config.overflow_mode {
+            OverflowBehaviour::Wrap => Ok(fst.wrapping_add(snd)),
+            OverflowBehaviour::Saturate => Ok(fst.saturating_add(snd)),
+            OverflowBehaviour::Trap => fst
+                .checked_add(snd)
+                .ok_or_else(|| anyhow!("overflow error",)),
+        }
+    }
+
+    /// Left shifts while respecting the configuration for overflow behaviour.
+    fn shift_left(&self, fst: i32, shamt: u32) -> anyhow::Result<i32> {
+        let (res, overflowed) = fst.overflowing_shl(shamt);
+        match self.config.overflow_mode {
+            OverflowBehaviour::Trap if overflowed => Err(anyhow!("overflow error")),
+            _ => Ok(res),
+        }
+    }
+
+    /// Logical right shifts while respecting the configuration for overflow behaviour.
+    fn shift_right_logical(&self, fst: i32, shamt: u32) -> anyhow::Result<i32> {
+        // right shifts are arithmetic on signed integers and logical on unsigned integers
+        let (res, overflowed) = (fst as u32).overflowing_shr(shamt);
+        match self.config.overflow_mode {
+            OverflowBehaviour::Trap if overflowed => Err(anyhow!("overflow error")),
+            _ => Ok(res as i32),
+        }
+    }
+
+    /// Arithmetic right shifts while respecting the configuration for overflow behaviour.
+    fn shift_right_arithmetic(&self, fst: i32, shamt: u32) -> anyhow::Result<i32> {
+        // right shifts are arithmetic on signed integers and logical on unsigned integers
+        let (res, overflowed) = fst.overflowing_shr(shamt);
+        match self.config.overflow_mode {
+            OverflowBehaviour::Trap if overflowed => Err(anyhow!("overflow error")),
+            _ => Ok(res),
+        }
+    }
+
+    pub fn apply(&mut self, update: Update) -> anyhow::Result<()> {
+        self.pc = update.nextpc;
+        match update.diff {
+            Some(Diff::Memory { addr, val, op }) => {
+                self.memory.store(addr, val, op)?;
+            }
+            Some(Diff::Register { reg, val }) => self.regfile[reg] = val,
+            None => (),
+        };
+        Ok(())
+    }
+
+    pub fn execute(&mut self) -> Option<anyhow::Result<Update>> {
+        if self.program.at(self.pc).is_none() {
+            None
+        } else {
+            Some(self._execute())
+        }
+    }
+
+    // Should only be called if the program is not done executing. This is
+    // so that we can use the ? in the Result monad. Self::execute is just a thin
+    // wrapper around this function that first checks if we are done executing,
+    // and as such returns an Option<Result<Update>> - making the ? only useful
+    // for options.
+    fn _execute(&mut self) -> anyhow::Result<Update> {
+        // See contract for calling funtion
+        let asm = self
+            .program
+            .at(self.pc)
+            .expect("program is not done executing");
+
+        // If necessary, take a snapshot _before_ proceeding
+        if self.executed % SNAPSHOT_INTERVAL == 0 {
+            if let Some(prev) = self.snapshots.insert(self.executed, self.regfile.clone()) {
+                // Sanity check that that we're still computing the same result
+                // this time around
+                assert_eq!(prev, self.regfile);
+            }
+        }
+        // TODO: move the state changes after execution in case there was an error
+        self.executed += 1;
+
+        let regs = &self.regfile;
+        let pc = self.pc;
+
+        // Helper functions for producing the update
+        // Advance the pc by 4 and change the appropriate register
+        let next_with = |reg, val| Update {
+            nextpc: pc + 4,
+            diff: Some(Diff::Register { reg, val }),
+        };
+
+        // Advance the pc by 4 and change the appropriate memory location
+        let next_mem = |addr, val, op| Update {
+            nextpc: pc + 4,
+            diff: Some(Diff::Memory { addr, val, op }),
+        };
+
+        // Just advance the pc
+        let next = Update {
+            nextpc: pc + 4,
+            diff: None,
+        };
+
+        // Some notes:
+        // * we mask shift amounts
+        let update = match *asm {
+            // TODO: make sure to test arithmetic/logical shifting
+            Instruction::RegImm { rd, r1, imm, op } => {
+                let r1val = regs[r1];
+                let val = match op {
+                    RegImmOp::Addi => self
+                        .add(r1val, imm)
+                        .with_context(|| format!("failed to add: {r1} = {r1val}, imm = {imm}"))?,
+                    RegImmOp::Sltiu => ((r1val as u32) < (imm as u32)) as i32,
+                    RegImmOp::Slli => self.shift_left(r1val, imm as u32).with_context(|| {
+                        format!("failed to left shift: {r1} = {r1val}, imm = {imm}",)
+                    })?,
+                    RegImmOp::Srli => {
+                        self.shift_right_logical(r1val, imm as u32)
+                            .with_context(|| {
+                                format!(
+                                    "failed to logical right shift: {r1} = {r1val}, imm = {imm}",
+                                )
+                            })?
+                    }
+                    RegImmOp::Srai => {
+                        self.shift_right_arithmetic(r1val, imm as u32)
+                            .with_context(|| {
+                                format!(
+                                    "failed to arithmetic right shift: {r1} = {r1val}, imm = {imm}",
+                                )
+                            })?
+                    }
+                    RegImmOp::Slti => (regs[r1] < imm) as i32,
+                    RegImmOp::Xori => regs[r1] ^ imm,
+                    RegImmOp::Ori => regs[r1] | imm,
+                    RegImmOp::Andi => regs[r1] & imm,
+                };
+                next_with(rd, val)
+            }
+            Instruction::RegReg { rd, r1, r2, op } => {
+                let r1val = regs[r1];
+                let r2val = regs[r2];
+                let val = match op {
+                    RegRegOp::Add => self.add(r1val, r2val).with_context(|| {
+                        format!("failed to add: {r1} = {r1val}, {r2} = {r2val}")
+                    })?,
+                    RegRegOp::Sub => self.add(r1val, -r2val).with_context(|| {
+                        format!("failed to subtract: {r1} = {r1val}, {r2} = {r2val}")
+                    })?,
+                    RegRegOp::Sll => self.shift_left(r1val, r2val as u32).with_context(|| {
+                        format!("failed to left shift: {r1} = {r1val}, {r2} = {r2val}")
+                    })?,
+                    RegRegOp::Srl => {
+                        self.shift_right_logical(r1val, r2val as u32)
+                            .with_context(|| {
+                                format!(
+                                    "failed to logical right shift: {r1} = {r1val}, {r2} = {r2val}",
+                                )
+                            })?
+                    }
+
+                    RegRegOp::Sra => self
+                        .shift_right_arithmetic(r1val, r2val as u32)
+                        .with_context(|| {
+                            format!(
+                                "failed to arithmetic right shift: {r1} = {r1val}, {r2} = {r2val}",
+                            )
+                        })?,
+                    RegRegOp::Sltu => ((r1val as u32) + (r2val as u32)) as i32,
+                    RegRegOp::Slt => (r1val < r2val) as i32,
+                    RegRegOp::Xor => r1val ^ r2val,
+                    RegRegOp::Or => r1val | r2val,
+                    RegRegOp::And => r1val & r2val,
+                };
+                next_with(rd, val)
+            }
+            Instruction::Load { rd, offset, r1, op } => {
+                let addr = self.add(offset, regs[r1]).with_context(|| {
+                    format!(
+                        "failed to calculate address: {r1} = {}, offset = {offset}",
+                        regs[r1]
+                    )
+                })?;
+                next_with(
+                    rd,
+                    self.memory
+                        .load(addr, op)
+                        .context("failed to perform load")?,
+                )
+            }
+            Instruction::Store { r2, offset, r1, op } => {
+                let addr = self.add(offset, regs[r1]).with_context(|| {
+                    format!(
+                        "failed to calculate address: {r1} = {}, offset = {offset}",
+                        regs[r1]
+                    )
+                })?;
+                next_mem(addr, regs[r2], op)
+            }
+            Instruction::Branch { r1, r2, label, op } => {
+                let jump = match op {
+                    BranchOp::Beq => regs[r1] == regs[r2],
+                    BranchOp::Bne => regs[r1] != regs[r2],
+                    BranchOp::Blt => regs[r1] < regs[r2],
+                    BranchOp::Bge => regs[r1] >= regs[r2],
+                    BranchOp::Bltu => (regs[r1] as u32) < (regs[r2] as u32),
+                    BranchOp::Bgeu => (regs[r1] as u32) >= (regs[r2] as u32),
+                    BranchOp::Bgt => regs[r1] > regs[r2],
+                    BranchOp::Ble => regs[r1] <= regs[r2],
+                    BranchOp::Bgtu => (regs[r1] as u32) > (regs[r2] as u32),
+                    BranchOp::Bleu => (regs[r1] as u32) <= (regs[r2] as u32),
+                };
+                if jump {
+                    Update::jump(self.program.label(label).unwrap().1)
+                } else {
+                    next
+                }
+            }
+            Instruction::BranchZero { r1, label, op } => {
+                let jump = match op {
+                    BranchZeroOp::Beqz => regs[r1] == 0,
+                    BranchZeroOp::Bnez => regs[r1] != 0,
+                    BranchZeroOp::Bltz => regs[r1] < 0,
+                    BranchZeroOp::Bgez => regs[r1] >= 0,
+                    BranchZeroOp::Bgtz => regs[r1] > 0,
+                    BranchZeroOp::Blez => regs[r1] <= 0,
+                };
+                if jump {
+                    Update::jump(self.program.label(label).unwrap().1)
+                } else {
+                    next
+                }
+            }
+            Instruction::LoadImm { rd, imm, op } => {
+                let val = match op {
+                    LoadImmOp::Lui => imm << 12,
+                    LoadImmOp::Li => imm,
+                };
+                next_with(rd, val)
+            }
+            Instruction::Unary { rd, r1, op } => {
+                let r1val = regs[r1];
+                let val = match op {
+                    UnaryOp::Mv => r1val,
+                    UnaryOp::Not => !r1val,
+                    UnaryOp::Neg => -r1val,
+                };
+                next_with(rd, val)
+            }
+            Instruction::call { label } => Update {
+                nextpc: self.program.label(label).unwrap().1,
+                diff: Some(Diff::Register {
+                    reg: Register::ra,
+                    val: self.pc + 4,
+                }),
+            },
+            Instruction::jal { rd, label } => Update {
+                nextpc: self.program.label(label).unwrap().1,
+                diff: Some(Diff::Register {
+                    reg: rd,
+                    val: self.pc + 4,
+                }),
+            },
+
+            Instruction::jalr { rd, offset, r1 } => Update {
+                nextpc: self.add(regs[r1], offset).with_context(|| "")? & !1,
+                diff: Some(Diff::Register {
+                    reg: rd,
+                    val: self.pc + 4,
+                }),
+            },
+            Instruction::j { label } => Update::jump(self.program.label(label).unwrap().1),
+            Instruction::jr { rs } => Update::jump((regs[rs] & !1) * 2),
+            Instruction::ret {} => Update::jump(regs[Register::ra]),
+        };
+        Ok(update)
+    }
+
+    fn revert(&mut self) -> Option<Update> {
+        todo!()
+    }
+}
